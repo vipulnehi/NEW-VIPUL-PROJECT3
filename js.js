7788faa880d7d6d50b55b87c1d43eeb0b0ca44f9
Vipul negi <158534215+vipulnehi@users.noreply.github.com>
2024-03-16 07:10:38 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,33 @@
+console.log("hellow world");
+// arithmatic operater
+let a=3;
+let b=4;
+let c=a+b;
+console.log("a+b=", a+b);
+console.log("a-b=", a-b);
+console.log("a=", a,"&b =",b);
+console.log("a*b=", a*b);
+
+
+
+console.log("a/b",a/b);
+
+
+
+// unary operator
+let d =5;
+let e =2;
+console.log("a =",d,"&b =", e );
+d++;
+e--;
+console.log(e);
+console.log(d);
+ let f=5;
+ let g=8;
+ console.log("++f=",++f, "& g++=",g++);
+console.log("g=",g)
+// assignment operator
+let h=4;
+let i=9;
+a*=4;
+console.log("a=",a);d
